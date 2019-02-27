94af0156c8af6fa325ba9beba0c0b72643e5d23f
justinevron <47427722+justinevron@users.noreply.github.com>
2019-02-27 16:20:34 -0500

    optimized

--- fancy_counter.vhd
+++ fancy_counter.vhd
@@ -40,37 +40,42 @@ entity fancy_counter is
            rst : in STD_LOGIC;
            updn : in STD_LOGIC;
            val : in STD_LOGIC_VECTOR (3 downto 0);
-           cnt : inout STD_LOGIC_VECTOR (3 downto 0));
+           cnt : out STD_LOGIC_VECTOR (3 downto 0));
 end fancy_counter;
 
 architecture Behavioral of fancy_counter is
 signal dir_reg : std_logic;
 signal value : std_logic_vector(3 downto 0);
+signal count : std_logic_vector(3 downto 0);
 begin
+cnt <= count;
 process(clk)
 begin
-    if en = '1' then
-        if rising_edge(clk) and rst = '1' then
-            cnt <= "0000";
+if rising_edge(clk)and en = '1' then
+        if rst = '1' then
+            count <= "0000";
         end if;
         if clk_en = '1' then
-            if rising_edge(clk) then
                 if updn = '1' then
                     dir_reg <= dir;
                 end if;
                 if ld = '1' then
                    value <= val;
                 end if;
-                if dir_reg = '1' and cnt < value then
-                    cnt <= STD_LOGIC_VECTOR(unsigned(cnt) +1);
-                elsif dir_reg = '1' and cnt = value then
-                    cnt <= "0000";    
-                elsif dir_reg = '0' and cnt > "0000" then
-                     cnt <= STD_LOGIC_VECTOR(unsigned(cnt) -1);
-                elsif dir_reg = '0' and cnt = "0000" then
-                     cnt <= value;
+                if dir_reg = '1' then
+                    if count < value then
+                        count <= STD_LOGIC_VECTOR(unsigned(count) +1);
+                    else
+                        count <= "0000"; 
+                    end if;
+   
+                elsif dir_reg = '0' then
+                    if count > "0000" then
+                         count <= STD_LOGIC_VECTOR(unsigned(count) -1);
+                    else
+                         count <= value;
+                    end if;
                 end if;
-            end if;
         end if;
     end if;
 end process;
